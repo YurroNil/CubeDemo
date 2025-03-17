96c141e27eec7654753d8a48e71673c32bb23e0f
Yorako <retyzlic@qq.com>
2025-03-17 20:35:37 +0800

    1.0.1 Release
    热修了一波依赖库丢失的问题

--- build/compile.ps1
+++ build/compile.ps1
@@ -1,2 +1,2 @@
 cd ../
-g++.exe -fexec-charset=gbk -g src/*.cpp src/core/*.cpp src/rendering/*.cpp -o ./bin/Demo.exe -I"./include" -I"./include/tplib" -L"./lib" -lglfw3 -lopengl32 -lgdi32
+g++.exe -fexec-charset=gbk -g src/*.cpp src/core/*.cpp src/rendering/*.cpp -o ./bin/Demo.exe -I"./include" -I"./include/tplib" -L"./lib" -lglfw3 -lopengl32 -lgdi32 -static
